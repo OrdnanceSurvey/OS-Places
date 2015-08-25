af91bbd0cbea31b209540c40b3453b5422647354
Gord <michaeljohngordon@gmail.com>
2015-08-25 13:25:22 +0000

    Renamed files and added smallest Typeahead example

---
+++ Code_snippets/Python3/urlib_find.py
@@ -0,0 +1,22 @@
+import urllib
+
+url = 'https://api.ordnancesurvey.co.uk/places/v1/addresses/radius?%s'
+params = urllib.urlencode({'point':'437318.0,115539.0','radius':'150','dataset':'DPA,LPI', 'key':'INSERT_YOUR_API_KEY_HERE'})
+
+try:
+    f = urllib.urlopen(url % params)
+except Exception as e:
+    print(str(e))
+    
+response=f.read()
+#print 'RESPONSE:', response
+
+
+for line in response.splitlines():
+    
+    word_lst = line.split(':')
+    for word in word_lst:
+        if '"ADDRESS" ' in word: print(line)
+        if 'COORDINATE' in word: print(line)  
+        if 'UPRN' in word: print(line)
+f.close()
---
+++ Code_snippets/Typeahead_find/typeahead_find.js
@@ -0,0 +1,39 @@
+var places = new Bloodhound({
+    datumTokenizer: function(d) {
+        return Bloodhound.tokenizers.whitespace(d.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        url: 'https://api.ordnancesurvey.co.uk/places/v1/addresses/find?query=%QUERY&key=cSUnSnxJXhibWYPqaC58MlSqqJldByak',
+        filter: function(places) {
+            // Map the remote source JSON array to a JavaScript object array
+            return $.map(places.results, function(address) {
+                return {
+                    ADDRESS: address.DPA.ADDRESS,
+                    X_COORDINATE: address.DPA.X_COORDINATE,
+                    Y_COORDINATE: address.DPA.Y_COORDINATE,
+                    value: address,
+                    UPRN: address.DPA.UPRN
+                };
+            });
+        }
+    }
+});
+
+places.initialize();
+
+$('.typeahead').typeahead({
+    highlight: true
+}, {
+    name: 'Address',
+    displayKey: 'ADDRESS',
+    source: places.ttAdapter(),
+    templates: {
+        empty: [
+            '<div class="tt-empty-message">',
+            'No Results',
+            '</div>'
+        ].join('\n'),
+        header: '<h4 class="tt-tag-heading tt-tag-heading2"> OS Places</h4>'
+    }
+});
