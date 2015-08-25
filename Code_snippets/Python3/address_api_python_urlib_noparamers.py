5970e8f4651126029003e15f1af2d9667576a20a
Gord <michaeljohngordon@gmail.com>
2015-08-25 13:24:53 +0000

    Renamed files and added smallest Typeahead example

--- Code_snippets/Python3/address_api_python_urlib_noparamers.py
+++
@@ -1,6 +0,0 @@
-import urllib
-opener = urllib.FancyURLopener({})
-url = 'https://api.ordnancesurvey.co.uk/places/v1/addresses/postcode?postcode=so16%206hw&dataset=DPA,LPI&key=INSERT_YOUR_API_KEY_HERE'
-f = opener.open(url)
-response=f.read()
-print ( response )
